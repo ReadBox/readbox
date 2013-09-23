9001e550c368690f7e071cb18d95e1ec681f59de
WoLpH <Rick@Fawo.nl>
2013-09-23 12:39:26 +0200

    updated admin

--- readbox/admin.py
+++ readbox/admin.py
@@ -37,7 +37,42 @@ class RevisionAdmin(admin.ModelAdmin):
     search_fields = ('path',)
 
 
+class FilePatternAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = (u'id', 'updated_at', 'created_at', 'name')
+    list_filter = ('updated_at', 'created_at')
+    raw_id_fields = ('patterns',)
+    search_fields = ('name',)
+
+
+class PatternAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = (u'id', 'updated_at', 'created_at', 'name', 'pattern',
+         'example', 'child_pattern')
+    list_filter = ('updated_at', 'created_at', 'child_pattern')
+    search_fields = ('name',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = (u'id', 'updated_at', 'created_at', 'name')
+    list_filter = ('updated_at', 'created_at')
+    search_fields = ('name',)
+
+
+class TagTypeAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = (u'id', 'updated_at', 'created_at', 'name', 'slug',
+         'description', 'color')
+    list_filter = ('updated_at', 'created_at')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name', 'slug')
+
 admin.site.register(models.File, FileAdmin)
+admin.site.register(models.FilePattern, FilePatternAdmin)
+admin.site.register(models.Pattern, PatternAdmin)
 admin.site.register(models.Permission, PermissionAdmin)
 admin.site.register(models.Revision, RevisionAdmin)
+admin.site.register(models.Tag, TagAdmin)
+admin.site.register(models.TagType, TagTypeAdmin)
 
