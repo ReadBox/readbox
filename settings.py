e87bd1406d1ae934a2f0d3714c482437bd0b18f1
WoLpH <Rick@Fawo.nl>
2013-09-25 11:55:24 +0200

    updated requirements

--- settings.py
+++ settings.py
@@ -140,6 +140,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'tastypie',
     'south',
     'provider',
     'provider.oauth2',
