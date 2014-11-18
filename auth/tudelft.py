391aba9f74d38c44763d10b9d4c2678fcbd38499
Rick van Hattem <Wolph@wol.ph>
2014-11-18 11:13:43 +0100

    not sure why but apparently the TU decided to change the API rights... disabling the last option to identify people

--- auth/tudelft.py
+++ auth/tudelft.py
@@ -7,17 +7,25 @@ class OAuth2Backend(oauth.BaseOAuth2):
     AUTHORIZATION_URL = 'https://oauth.tudelft.nl/oauth2/authorize'
     ACCESS_TOKEN_URL = 'https://oauth.tudelft.nl/oauth2/token'
     ACCESS_TOKEN_METHOD = 'POST'
+    SCOPE = 'cijfers'
+
+    #def get_json(self, url, *args, **kwargs):
+    #    response = self.request(url, *args, **kwargs)
+    #    text = response.text
+    #    assert text
+    #    return response.json()
 
     def get_user_details(self, response):
-        data = response['studievoortgang'][0]
-        return {'username': data['studentnummer']}
+        return {}
+        #data = response['studievoortgang'][0]
+        #return {'username': data['studentnummer']}
 
-    def user_data(self, access_token, *args, **kwargs):
-        url = 'https://api.tudelft.nl/v0/studievoortgang'
-        data = self.get_json(url, params={'oauth_token': access_token})
-        return data['getStudievoortgangByStudentnummerResponse']
+    #def user_data(self, access_token, *args, **kwargs):
+    #    url = 'https://api.tudelft.nl/v0/studievoortgang'
+    #    data = self.get_json(url, params={'oauth_token': access_token})
+    #    return data['getStudievoortgangByStudentnummerResponse']
 
-    def get_user_id(self, details, response):
-        data = response['studievoortgang'][0]
-        return data['studentnummer']
+    #def get_user_id(self, details, response):
+    #    data = response['studievoortgang'][0]
+    #    return data['studentnummer']
 
