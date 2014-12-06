bbb61ffbe90863abee58b36a27e5bf727171426c
Rick van Hattem <Wolph@wol.ph>
2014-12-06 16:46:16 +0100

    tu delft sucks so much... crappy changing api with virtually no docs...

--- auth/tudelft.py
+++ auth/tudelft.py
@@ -9,23 +9,10 @@ class OAuth2Backend(oauth.BaseOAuth2):
     ACCESS_TOKEN_METHOD = 'POST'
     SCOPE = 'cijfers'
 
-    #def get_json(self, url, *args, **kwargs):
-    #    response = self.request(url, *args, **kwargs)
-    #    text = response.text
-    #    assert text
-    #    return response.json()
-
     def get_user_details(self, response):
         return {}
-        #data = response['studievoortgang'][0]
-        #return {'username': data['studentnummer']}
 
-    #def user_data(self, access_token, *args, **kwargs):
-    #    url = 'https://api.tudelft.nl/v0/studievoortgang'
-    #    data = self.get_json(url, params={'oauth_token': access_token})
-    #    return data['getStudievoortgangByStudentnummerResponse']
-
-    #def get_user_id(self, details, response):
-    #    data = response['studievoortgang'][0]
-    #    return data['studentnummer']
+    def get_user_id(self, details, response):
+        return {'username': 'TU Delft random user: ' +
+                response['access_token']}
 
